dd3758b736311a3bfae689308c08832ea2a9b2d8
Ankush <ankushnitjsr@gmail.com>
2023-04-11 11:34:11 +0530

    Encapsulation in Java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rw = new RWOnly();
+        //rw.name = "Ankush";
+        //System.out.println(rw.name); // Error : java: name has private access in com.driver.RWOnly
+        rw.setName("Ankush");
+        System.out.println(rw.getName());
+    }
 }
